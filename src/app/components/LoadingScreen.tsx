3cc96d951f8de72da619e0d746998bbc0a8e7993
Minu248 <wkdalsdn5656@gmail.com>
2025-07-21 20:13:14 +0900

    로딩 화면 배경색 수정

--- src/app/components/LoadingScreen.tsx
+++ src/app/components/LoadingScreen.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { TOAST_MESSAGES } from "@/config/constants";
 
 export const LoadingScreen: React.FC = () => (
-  <main className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-[#FF2A68] via-[#A033FF] to-[#0B63F6] px-4">
+  <main className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-[#060261] via-[#A901FD] to-[#07002F] px-4">
     <div className="flex flex-col items-center justify-center">
       <div className="w-64 h-64 md:w-[512px] md:h-[512px] mb-8">
         <img 
